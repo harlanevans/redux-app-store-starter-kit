c4201289060164d347a2d93c9834d448adb97b1b
Harlan Evans <harlanevans5@gmail.com>
2018-12-18 12:32:36 -0700

    went through lecture, missing some code, need fetch from spencer

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Home from './components/Home';
 import NoMatch from './components/NoMatch';
 import Navbar from './components/Navbar';
 import Apps from './components/Apps';
+import FetchApps from './components/FetchApps';
+import AppView from './components/AppView';
 import { Container, } from "semantic-ui-react";
 import { Route, Switch, } from "react-router-dom";
 
@@ -12,7 +14,7 @@ const App = () => (
     <Container>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/apps" component={Apps} />
+        <Route path='/apps' component={FetchApps} />
         <Route component={NoMatch} />
       </Switch>
     </Container>
--- client/src/reducers/apps.js
+++ client/src/reducers/apps.js
@@ -1,23 +1,59 @@
 import axios from 'axios';
+import { StatisticLabel } from 'semantic-ui-react';
 
-export const getApps = () => {
+const APPS = 'APPS';
+const ADD_APP = 'ADD_APP';
+const UPDATE_APP = 'UPDATE_APP';
+const DELETE_APP = 'DELETE_APP';
+
+// Redux Actions
+
+export const getApps = (cb) => {
   return (dispatch) => {
     axios.get('/api/apps')
-    .then( res => dispatch({ type: "APPS", apps: res.data }))
+      .then(res => dispatch({ type: APPS, apps: res.data }))
+      .then( cb() )
   }
 }
 
-export default (store = [], action ) => {
-  switch(action.type) {
-    case "APPS":
+export const addApp = (app) => {
+  return (dispatch) => {
+    axios.post('./api/apps', { app, })
+      .then(res => dispatch({ type: ADD_APP, app: res.data }))
+  }
+}
+
+export const updateApp = (app) => {
+  return (dispatch) => {
+    axios.put(`./api/apps/${app.id}`, { app, })
+      .then(res => dispatch({ type: UPDATE_APP, app: res.data, }))
+  }
+}
+
+export const deleteApp = (id) => {
+  return (dispatch) => {
+    axios.delete(`/api/apps/${id}`)
+    .then( res => dispatch({ type: DELETE_APP, id, }))
+  }
+}
+
+// Redux Reducer
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case APPS:
       return action.apps;
-    case "ADD_APP":
-
-    case "UPDATE_APP":
-
-    case "DELETE_APP":
-
+    case ADD_APP:
+      return [action.app, ...state];
+    case UPDATE_APP:
+      return state.map(app => {
+        if (app.id === action.app.id)
+          return action.app;
+        return app;
+      })
+    case DELETE_APP:
+      return state.filter( a => a.id !== action.id );
     default:
-    return store;
+      return state;
   }
 }
